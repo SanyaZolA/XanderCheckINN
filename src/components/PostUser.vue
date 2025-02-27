b4cd98bb602c575cc59952bff87bb9e97c3c2f3a
Alexandr Sergeev <ntl1221@yandex.ru>
2025-02-27 20:28:27 +0300

    fix PostUser

--- src/components/PostUser.vue
+++ src/components/PostUser.vue
@@ -5,10 +5,6 @@
       <input v-model="data.requestDate" type="date" placeholder="Введите дату (формата 2012-01-25)" style="width: 222px"/>
       <button type="submit">Проверить</button>
     </form>
-    <div v-if="response" class="answer">
-      <h2>Ответ:</h2>
-      <pre class="pre">{{ response }}</pre>
-    </div>
     <div v-if="error" class="answer" style="color: red">
       <h2>Ошибка:</h2>
       <pre class="pre">{{ error }}</pre>
@@ -30,7 +26,6 @@ export default {
         inn: '',
         requestDate: ''
       },
-      response: null,
       error: null,
       trues: null
     };
@@ -42,15 +37,12 @@ export default {
         if (res.status === 200) {
           if (res.data.code === 'validation.failed') {
             this.error = res.data.message;
-            this.response = null;
             this.trues = null;
           } else if (res.data.status === false) {
             this.error = res.data.message;
-            this.response = null;
             this.trues = null;
           } else if (res.data.status === true) {
             this.trues = res.data.message;
-            this.response = null;
             this.error = null;
           } else {
             this.response = res.data;
@@ -66,7 +58,6 @@ export default {
         } else {
           this.error = error.message;
         }
-        this.response = null;
         this.trues = null; 
       }
     }
